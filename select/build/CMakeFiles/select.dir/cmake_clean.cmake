de4c281905af7776c1ffa67e409a6753ea2689f9
xxx <1250855168@qq.com>
2022-12-04 21:51:31 +0800

    updata

---
+++ select/build/CMakeFiles/select.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/select.dir/main.c.o"
+  "CMakeFiles/select.dir/src/socket.c.o"
+  "select"
+  "select.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/select.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
